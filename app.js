9276b3358cfa74396d743d129a72b8a7df044a77
Chao Zhou <Czhou0k@gmail.com>
2023-02-13 22:16:36 -0500

    added arrays methods

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ const x = null;
 const y = undefined;
 
 // Concatenation old way
-console.log('My name is ' + name + ' and I am ' + age);
+// console.log('My name is ' + name + ' and I am ' + age);
 
 //Concatenation new way Temperal Literals/Template String
-console.log(`My name is ${name} and I am ${age}`);
+// console.log(`My name is ${name} and I am ${age}`);
 
 //String property and methods
 
 const string = 'Hello World';
 
-console.log(string.length);
-console.log(string.toUpperCase());
-console.log(string.substring(0, 5));
-console.log(string.substring(0, 5).toUpperCase());
+// console.log(string.length);
+// console.log(string.toUpperCase());
+// console.log(string.substring(0, 5));
+// console.log(string.substring(0, 5).toUpperCase());
+// console.log(string.split(''));
+
+//Arrays
+
+const numbers = new Array(1, 2, 3, 4, 5);
+
+const fruits = ['apples', 'oranges', 'bananas', 'pears'];
+
+console.log(fruits[1]);
