a79e735669000a66dc65e1a0d2fffcddb4ffa11b
Chao Zhou <Czhou0k@gmail.com>
2023-02-15 20:25:25 -0500

    added innerText and innterHTML example

--- main.js
+++ main.js
@@ -14,3 +14,6 @@ const ul = document.querySelector('.items');
 // ul.lastElementChild.remove();
 
 ul.firstElementChild.textContent = 'How Are You?';
+// ul.children[1].textContent = 'Fine';
+ul.children[1].innerText = 'Fine';
+ul.lastElementChild.innerHTML = '<h1>WE ARE ONE PIECE</h1>';
