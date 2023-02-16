487057719caeb4be5f537a1f8e86b80242573b62
Chao Zhou <Czhou0k@gmail.com>
2023-02-15 20:27:28 -0500

    added a style to btn by selector

--- main.js
+++ main.js
@@ -10,10 +10,13 @@ const form = document.getElementById('my-form');
 
 // items.forEach((item) => console.log(item));
 
-const ul = document.querySelector('.items');
+// const ul = document.querySelector('.items');
 // ul.lastElementChild.remove();
 
-ul.firstElementChild.textContent = 'How Are You?';
+// ul.firstElementChild.textContent = 'How Are You?';
 // ul.children[1].textContent = 'Fine';
-ul.children[1].innerText = 'Fine';
-ul.lastElementChild.innerHTML = '<h1>WE ARE ONE PIECE</h1>';
+// ul.children[1].innerText = 'Fine';
+// ul.lastElementChild.innerHTML = '<h1>WE ARE ONE PIECE</h1>';
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'blue';
